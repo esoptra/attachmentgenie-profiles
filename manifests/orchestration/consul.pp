82aed0b6e058308b83e1b60178ff23ef5da97134
Bram Vogelaar <attachmentgenie@inuits.eu>
2017-11-07 14:51:11 +0100

    turning on option to add consul to resolv.conf using dnsmasq

--- manifests/orchestration/consul.pp
+++ manifests/orchestration/consul.pp
@@ -3,24 +3,30 @@
 # @example when declaring the consul class
 #  class { '::profiles::orchestration::consul': }
 #
-# @param checks   Consul checks,
-# @param config   Consul config,
-# @param options  Additional consul start up flags.
-# @param resolv   Configure resolv.conf to use consul.
-# @param services Consul services.
-# @param version  Version of consul to install.
-# @param watches  Consul watches.
+# @param checks       Consul checks,
+# @param config       Consul config,
+# @param domainname   Resolv.conf domain
+# @param name_servers Name servers to use in resolv.conf
+# @param options      Additional consul start up flags.
+# @param resolv       Configure resolv.conf to use consul.
+# @param searchpath   Resolv.conf searchpath.
+# @param services     Consul services.
+# @param version      Version of consul to install.
+# @param watches      Consul watches.
 class profiles::orchestration::consul (
-  Hash $checks    = {},
-  Hash $config    = {
+  Hash $checks = {},
+  Hash $config = {
     'data_dir'   => '/opt/consul',
     'datacenter' => 'vagrant',
   },
+  String $domainame = $::domain,
+  Array $name_servers = ['127.0.0.1'],
   String $options = '-enable-script-checks -syslog',
   Boolean $resolv = false,
-  Hash $services  = {},
-  String $version = '0.9.3',
-  Hash $watches   = {},
+  Array $searchpath = [],
+  Hash $services = {},
+  String $version = '1.0.0',
+  Hash $watches = {},
 ) {
   package { 'unzip':
     ensure => present,
@@ -43,8 +49,9 @@ class profiles::orchestration::consul (
     }
 
     class { 'resolv_conf':
-      domainname  => $::domain,
-      nameservers => ['127.0.0.1', '10.0.2.3'],
+      domainname  => $domainame,
+      nameservers => $name_servers,
+      searchpath  => $searchpath,
     }
   }
 }

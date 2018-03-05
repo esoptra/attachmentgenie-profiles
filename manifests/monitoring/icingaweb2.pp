54e5c874c4be334bbe0d4ff63970b044c79d42f1
Tom Vansant <jupike@gmail.com>
2018-03-05 10:40:37 +0100

    Update icingaweb2.pp

--- manifests/monitoring/icingaweb2.pp
+++ manifests/monitoring/icingaweb2.pp
@@ -1,6 +1,7 @@
 # Add a view only role for incingaweb2
-
+class profiles::monitoring::icingaweb2 (
    icingaweb2::config::role { 'View only':
       users       => 'Viewer',
       permissions => 'module/monitoring',
     }
+  )

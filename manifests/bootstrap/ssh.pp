9b1edadab0ebcc5fde0860e2273477f0f121beeb
Tom Vansant <jupike@gmail.com>
2018-04-10 09:47:54 +0200

    Add gatewayports string to profiles

--- manifests/bootstrap/ssh.pp
+++ manifests/bootstrap/ssh.pp
@@ -5,10 +5,12 @@
 #
 # @param allow_agent_forwarding  Allow forwarding of the agent.
 # @param forward_agent           Forward the connection of the agent to the remote machine.
+# @param gatewayports            Allow creating SSH tunnels
 # @param password_authentication Accept access using password.
 class profiles::bootstrap::ssh (
   String $allow_agent_forwarding  = 'no',
   String $forward_agent           = 'no',
+  String $gatewayports            = 'no',
   String $password_authentication = 'no',
 ) {
   class { '::ssh::client':

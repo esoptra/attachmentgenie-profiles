c44f85732e0396e63873338ea5b66b324a267e79
Nitish <33031798+Nitish-SH@users.noreply.github.com>
2018-04-11 14:46:25 +0200

    adding permit_tunnel

--- manifests/bootstrap/ssh.pp
+++ manifests/bootstrap/ssh.pp
@@ -5,7 +5,9 @@
 #
 # @param allow_agent_forwarding  Allow forwarding of the agent.
 # @param forward_agent           Forward the connection of the agent to the remote machine.
-# @param gatewayports            Allow creating SSH tunnels
+# @param gatewayports            Allow binding on any ip address for remote ssh tunnels
+# @param allow_tcp_forwarding    Allow creating remote SSH tunnels
+# @param permit_tunnel           Allow creating local SSH tunnels
 # @param password_authentication Accept access using password.
 class profiles::bootstrap::ssh (
   String $allow_agent_forwarding  = 'no',
@@ -13,6 +15,7 @@ class profiles::bootstrap::ssh (
   String $forward_agent           = 'no',
   String $gatewayports            = 'no',
   String $password_authentication = 'no',
+  String $permit_tunnel           = 'no',
 ) {
   class { '::ssh::client':
     forward_agent           => $forward_agent,
@@ -22,6 +25,7 @@ class profiles::bootstrap::ssh (
     allow_agent_forwarding   => $allow_agent_forwarding,
     allow_tcp_forwarding     => $allow_tcp_forwarding,
     password_authentication  => $password_authentication,
-    gateway_ports            => $gatewayports
+    gateway_ports            => $gatewayports,
+    permit_tunnel            => $permit_tunnel
   }
 }

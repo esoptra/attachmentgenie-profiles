4eaefa45b40812a70e83392e5f739a657e879546
Tom Vansant <jupike@gmail.com>
2018-04-13 14:47:57 +0200

    syntax correction

--- manifests/bootstrap/fail2ban.pp
+++ manifests/bootstrap/fail2ban.pp
@@ -7,10 +7,10 @@
 # @param subnets  Subnets to be whitelisted.
 class profiles::bootstrap::fail2ban (
   Array $services =['ssh', 'ssh-ddos'],
-  Array $subnets =['127.0.0.1/8','10.0.0.0/16','172.31.1.0/24','81.83.16.118/32','61.12.36.234/32'],
+  Array $subnets =['127.0.0.1/8', '10.0.0.0/16', '172.31.1.0/24', '81.83.16.118/32', '61.12.36.234/32'],
 ) {
   class { '::fail2ban':
-    jails => $services
-    whitelist => $subnets
+    jails     => $services,
+    whitelist => $subnets,
   }
 }

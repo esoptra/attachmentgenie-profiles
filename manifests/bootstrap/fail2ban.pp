6d7fe36c1bd4c271c6603ab65ee790c19b669fd2
Tom Vansant <jupike@gmail.com>
2018-04-13 11:05:23 +0200

    added whitelist parameter

--- manifests/bootstrap/fail2ban.pp
+++ manifests/bootstrap/fail2ban.pp
@@ -4,10 +4,13 @@
 #  class { '::profiles::bootstrap::fail2ban': }
 #
 # @param services Services to control.
+# @param subnets  Subnets to be whitelisted.
 class profiles::bootstrap::fail2ban (
   Array $services =['ssh', 'ssh-ddos'],
+  Array $subnets =['127.0.0.1/8','10.0.0.0/16','172.31.1.0/24','81.83.16.118/32','61.12.36.234/32'],
 ) {
   class { '::fail2ban':
     jails => $services
+    whitelist => $subnets
   }
 }

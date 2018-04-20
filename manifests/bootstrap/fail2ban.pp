bcd0d22f2a4fddf6706a72d999d329db8c4ce9be
Tom Vansant <jupike@gmail.com>
2018-04-20 13:21:02 +0200

    trying to get the whitelist to work

--- manifests/bootstrap/fail2ban.pp
+++ manifests/bootstrap/fail2ban.pp
@@ -9,8 +9,14 @@ class profiles::bootstrap::fail2ban (
   Array $services =['ssh', 'ssh-ddos'],
   Array $subnets =['127.0.0.1/8', '10.0.0.0/16', '172.31.1.0/24', '81.83.16.118/32', '61.12.36.234/32'],
 ) {
+  if $f2b {
+    class { 'fail2ban':
+      config_file_template => "fail2ban/${::lsbdistcodename}/etc/fail2ban/jail.conf.erb",
+    }
+  }
   class { '::fail2ban':
     jails     => $services,
     whitelist => $subnets,
   }
 }
+    

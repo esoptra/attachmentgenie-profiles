4abf4957cf1923f28764e3aea8b48086abf974e1
Tom Vansant <jupike@gmail.com>
2018-04-20 13:28:38 +0200

    added parameter definition

--- manifests/bootstrap/fail2ban.pp
+++ manifests/bootstrap/fail2ban.pp
@@ -5,9 +5,11 @@
 #
 # @param services Services to control.
 # @param subnets  Subnets to be whitelisted.
+# @param f2b      Load config file
 class profiles::bootstrap::fail2ban (
   Array $services =['ssh', 'ssh-ddos'],
   Array $subnets =['127.0.0.1/8', '10.0.0.0/16', '172.31.1.0/24', '81.83.16.118/32', '61.12.36.234/32'],
+  Boolean $f2b = false,
 ) {
   if $f2b {
     class { 'fail2ban':

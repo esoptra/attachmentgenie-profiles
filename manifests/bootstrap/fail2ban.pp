78d9ffcd293c3a5ae48592d0fc3944c4f6b5492c
Tom Vansant <jupike@gmail.com>
2018-04-23 08:21:28 +0200

    update fail2ban

--- manifests/bootstrap/fail2ban.pp
+++ manifests/bootstrap/fail2ban.pp
@@ -6,18 +6,16 @@
 # @param services Services to control.
 # @param subnets  Subnets to be whitelisted.
 # @param f2b      Load config file
-#class profiles::bootstrap::fail2ban (
-#  Array $services =['ssh', 'ssh-ddos'],
+class profiles::bootstrap::fail2ban (
+  Array $services =['ssh', 'ssh-ddos'],
 #  Array $subnets =['127.0.0.1/8', '10.0.0.0/16', '172.31.1.0/24', '81.83.16.118/32', '61.12.36.234/32'],
 #  Boolean $f2b = false,
-#) {
-#  }
-#  class { '::fail2ban': 
+) { class { '::fail2ban':
 #    if $f2b {
-#      config_file_template => "fail2ban/${::lsbdistcodename}/etc/fail2ban/jail.conf.erb",
+      config_file_template => "fail2ban/${::lsbdistcodename}/etc/fail2ban/jail.conf.erb",
 #    }
 #    jails     => $services,
 #    whitelist => $subnets,
-#  }
-#}
-    
+  }
+}
+

5c91bc50fc9a31282ac7aa2f3a222be81488986f
Tom Vansant <jupike@gmail.com>
2018-05-07 09:14:48 +0200

    removed icingaweb2 from monitoring profile

--- manifests/monitoring/icingaweb2.pp
+++
@@ -1,7 +0,0 @@
-# Add a view only role for incingaweb2
-class profiles::monitoring::icingaweb2 (
-   icingaweb2::config::role { 'View only':
-      users       => 'Viewer',
-      permissions => 'module/monitoring',
-    }
-  )

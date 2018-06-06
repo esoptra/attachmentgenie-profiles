325e3fc2ff3a7243665d35754226b99dcfe3702c
Tom Vansant <jupike@gmail.com>
2018-06-06 09:10:35 +0200

    added bash completition short_hosts

---
+++ manifests/orchestration/bashcompletion.pp
@@ -0,0 +1,12 @@
+# This class is used to add a bash completition file.
+#
+#
+
+class short_hosts {
+
+  file { '/etc/bash_completion.d/short_hosts':
+    ensure  => 'file',
+    replace => 'yes',
+    source  => 'puppet:///modules/profiles/files/short_hosts',
+  }
+}

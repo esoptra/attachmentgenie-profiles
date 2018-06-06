af8c9af1c9483c458145e91527f004c292a455a2
Tom Vansant <jupike@gmail.com>
2018-06-06 10:08:47 +0200

    Syntax update

--- manifests/orchestration/bashcompletion.pp
+++ manifests/orchestration/bashcompletion.pp
@@ -1,12 +1,5 @@
 # This class is used to add a bash completition file.
 #
 #
-
-class profiles::orchestration::short_hosts {
-
-  file { '/etc/bash_completion.d/short_hosts':
-    ensure  => 'file',
-    replace => 'yes',
-    source  => 'puppet:///modules/profiles/files/short_hosts',
-  }
-}
+class profiles::orchestration::short_hosts (
+){class { '::short_hosts':}}
